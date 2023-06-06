b2e3156af101a6595fce3537f67cddc9a08cf597
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-06-06 10:24:03 -0500

    productos add

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      // in production, synchronize should be set to false
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+})
+export class AppModule {}
