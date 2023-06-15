b2e2e35e8df1e0e644dc8d7496179334cd096fd2
Jhon Bellido <jhonbellidovargas@gmail.com>
2023-06-15 12:45:15 -0500

    before update

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -61,8 +61,19 @@ export class ProductsService {
     return product;
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: string, updateProductDto: UpdateProductDto) {
+    const product: Product = await this.productRepository.preload({
+      id: id,
+      ...updateProductDto,
+    });
+    if (!product)
+      throw new NotFoundException(`Product with id ${id} not found`);
+
+    try {
+      return await this.productRepository.save(product);
+    } catch (error) {
+      this.handleException(error);
+    }
   }
 
   async remove(id: string) {
